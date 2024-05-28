9a761de6b2a493c3b5a904a725954835d5aa6fe3
AlexisMendez34 <166565669+AlexisMendez34@users.noreply.github.com>
2024-05-28 16:11:37 -0700

    Creé el repositorio

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native'
+import CategoriesScreen from "./screens/CategoriesScreens";
+import { StatusBar } from 'expo-status-bar';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+
+  
+  return (
+    <>
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
---
+++ screens/CategoriesScreens.js
@@ -0,0 +1,29 @@
+import { FlatList, StyleSheet } from "react-native";
+import { CATEGORIES } from "../data/dummy_data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+
+function CategoriesScreen({navigation}) {
+
+    function renderCategoryItem(itemData) {
+        function pressHandler() {
+            navigation.navigate('MealsOverview')
+        }
+
+        return (
+            <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler}/>
+        )
+    }
+
+    return (
+        <FlatList 
+            data={CATEGORIES} 
+            keyExtractor={(item) => item.id}
+            renderItem={renderCategoryItem}
+            numColumns={2}
+        />
+    )
+}
+
+
+export default CategoriesScreen
